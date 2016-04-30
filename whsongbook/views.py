c0fbc2861bb9b918ad36d72723e42fa3e49d8861
Jason Reeder <info@jasonreeder.com>
2016-04-30 02:03:53 -0700

    Add simple "songs" view prototype; add simple "song" template
     - At the moment, the "songs" view  is simply to test the app's
    ability to detect the songs in its "songs/production" folder.
    Later this should be built into a template listing those songs
    in an organized way.
    
     - The "song" template is now just to detect the contents of
    whichever song is specified in the url. Next those contents
    should be parsed into a dictionary to be fed into a template.

--- whsongbook/views.py
+++ whsongbook/views.py
@@ -1,5 +1,7 @@
 from . import app
 from flask import render_template
+from os import listdir
+import re
 
 @app.route("/")
 def home():
@@ -7,4 +9,20 @@ def home():
 
 @app.route("/songs")
 def songs():
-    return "Hello, songs"
+    """
+    List all songs in the production folder
+    """
+
+    songs_dir = "songs/production/"
+    return str(listdir(songs_dir))
+
+@app.route("/songs/<title>")
+def song(title):
+    # Test with "http://0.0.0.0:8080/songs/i_will_follow_you_into_the_dark-death_cab_for_cutie"
+
+    songs_dir = "songs/production/"
+    full_title = songs_dir + title + ".song"
+    with open(full_title, "r") as f:
+        # parse header
+        
+        return f.read()
