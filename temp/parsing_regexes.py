b6d984b027130735ab1d37a817364a67a94c8e8b
Jason Reeder <info@jasonreeder.com>
2016-05-10 17:41:49 -0700

    Add organizing comments

--- temp/parsing_regexes.py
+++ temp/parsing_regexes.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import ast
+import re
 
 FOLLOW = "../songs/production/i_will_follow_you_into_the_dark-death_cab_for_cutie.song"
 BAD_MOON = "../songs/production/bad_moon_rising-credence_clearwater_revival.song"
@@ -10,11 +11,15 @@ cur = None
 with open(BAD_MOON, "r") as f:
     text = f.read()
 for line in text.splitlines():
+    # strip initial white space
     line = line.rstrip()
+    # ignore blank lines
     if not line: continue
+    # identify line type statements (ie "chorus:")
     if line == line.lstrip():
         cur = []
         sections.append((line.strip(":"), cur))
+    # work with all remaining lines (content)
     else:
         cur.append(line.strip())
 
