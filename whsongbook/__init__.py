1b030d5bf5849c88ecfa710c222f2511eccf59ba
Jason Reeder <info@jasonreeder.com>
2016-05-10 20:37:20 -0700

    Add functions for displaying lyrics and chords
    These functions are referenced by the Jinja templates. They both render
    text in slightly changed forms. It is not possible to use these commands
    in templates, but these functions can be referenced, hence needing to
    add these to this file.
    
    There are two "display_lyrics" functions. I am still trying to figure
    out how to display two consecutive chords that have no lyrics between
    them. Currently they are shown next to each other without any spaces.
    These two functions are my attempts at fixing that, neither of which
    has been very successful.

--- whsongbook/__init__.py
+++ whsongbook/__init__.py
@@ -1,7 +1,34 @@
 import os
+import re
 
 from flask import Flask
 
 app = Flask(__name__)
 
 from . import views
+
+def check_type(data):
+    return isinstance(data, list)
+
+def display_lyrics(lyrics):
+    lyrics = lyrics or "\xA0"
+    if lyrics.endswith(" "):
+        lyrics = lyrics[:-1]+"\xA0"
+    return lyrics
+
+def display_lyrics(lyrics):
+    return lyrics.replace(" ", "\xA0") or "\xA0\xA0"
+
+def display_chords(chord):
+    note = re.sub("[0-9]|:.*", "", chord)
+    extension = ""
+    if ":" in chord:
+        pre, extension = chord.split(":")
+    ret = note.upper() + extension
+    return ret
+
+app.jinja_env.globals.update(
+    check_type = check_type,
+    display_lyrics = display_lyrics,
+    display_chords = display_chords
+)
