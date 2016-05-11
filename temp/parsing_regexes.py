533f370aca78b2946476c0836de241fd94f7434b
Jason Reeder <info@jasonreeder.com>
2016-05-10 17:36:54 -0700

    Clean up; add test file
    The script was no longer using the "text0" string - it was importing a
    file instead - so I deleted the "text0" string. I also added a second
    test file path.

--- temp/parsing_regexes.py
+++ temp/parsing_regexes.py
@@ -1,57 +1,14 @@
 from pprint import pprint
 import ast
 
-text0 = """
-header:
-    title = "I Will Follow You Into the Dark"
-    artist = "Death Cab for Cutie"
-    capo = 5
-    year = 2005
-    genre = ["indie"]
-
-instrumental:
-    <chorus chords>
-
-verse:
-    [c]Love of mine some day [a:m]you will die
-    But I'll be [f]close behind. I'll follow [c]you into the [g]dark,]
-    No [c]blinding light or tunnels to [a:m]gates of white]
-    Just our hands [f]clasped so tight, waiting [c]for the hint of a [g]spark.
-
-chorus:
-    If [a:m]Heaven and Hell de[c]cide that they [f]both are satisf[c]ied [g]
-    Ill[a:m]uminate the [c]"no"s on their [g]vacancy [g]signs
-    If [a:m]there's no one be[c]side you when your [e]soul em[a:m]barks [g]
-    Then [f]I'll follow [f:m]you into the dark [c] [c]
-
-verse:
-    In Catholic school as vicious as Roman rule
-    I got my knuckles bruised by a lady in black
-    And I held my tongue as she told me
-    "Son, fear is the heart of love." So I never went back
-
-chorus
-
-verse:
-    You and me have seen everything to see
-    From Bangkok to Calgary, and the soles of your shoes
-    Are all worn down. The time for sleep is now.
-    It's nothing to cry about, 'cause we'll hold each other soon
-
-bridge:
-    In the [a:m]blackest of [f]rooms [f] [f]
-
-chorus
-
-chorus:
-    and [f]I’ll follow [f:m]you into the [c]dark.
-"""
+FOLLOW = "../songs/production/i_will_follow_you_into_the_dark-death_cab_for_cutie.song"
+BAD_MOON = "../songs/production/bad_moon_rising-credence_clearwater_revival.song"
 
 sections = []
 metadata = {}
 cur = None
-with open("../songs/production/i_will_follow_you_into_the_dark-death_cab_for_cutie.song", "r") as f:
-    text1 = f.read()
+with open(BAD_MOON, "r") as f:
+    text = f.read()
 for line in text.splitlines():
     line = line.rstrip()
     if not line: continue
