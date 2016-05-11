c69732ca9595a617d60d6699d2aea13f3a715da0
Jason Reeder <info@jasonreeder.com>
2016-05-10 18:13:42 -0700

    Add chord parsing
    The old script returned lines with chords among them (eg "[c]Love of
    mine some day [a:m]you will die"). I think it would be best to parse
    out those chords before feeding them into the Jinja template, since
    Jinja templates do allow for full Python syntax. So this addition turns
    lines with chords into lists of tuples ({chord}, {lyrics}).

--- temp/parsing_regexes.py
+++ temp/parsing_regexes.py
@@ -10,19 +10,32 @@ metadata = {}
 cur = None
 with open(BAD_MOON, "r") as f:
     text = f.read()
+
 for line in text.splitlines():
     # strip initial white space
     line = line.rstrip()
     # ignore blank lines
     if not line: continue
+
     # identify line type statements (ie "chorus:")
     if line == line.lstrip():
         cur = []
         sections.append((line.strip(":"), cur))
+
     # work with all remaining lines (content)
     else:
-        cur.append(line.strip())
+        line = line.strip()
+        # separate chords from lyrics
+        if "[" in line and "genres = " not in line:
+            chord_sections = []
+            for section in line.split("["):
+                if "]" in section:
+                    chord, lyric = section.split("]")
+                    chord_sections.append((chord, lyric))
+            line = chord_sections
+        cur.append(line)
 
+# convert header section to dictionary
 if sections[0][0] == "header":
     for line in sections.pop(0)[1]:
         key, value = line.split('=', 1)
