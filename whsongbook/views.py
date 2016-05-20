fcbe0761406abd59fa9b7d48f7534f5aae7851a2
Jason Reeder <info@jasonreeder.com>
2016-05-19 21:59:54 -0700

    Remove song parsing
    Now that "__init__.py" does the song parsing, there is no longer any
    need to do it in "views.py". So I changed the functions in "views"
    to simply reference the parsed list created in "__init__".

--- whsongbook/views.py
+++ whsongbook/views.py
@@ -14,89 +14,33 @@ def about():
 
 @app.route("/random")
 def random():
-    songs_dir = "songs/production/"
-    songs = listdir(songs_dir)
-    selection = choice(songs)
+    selection = choice(songs_data).filename
     return redirect("/songs/%s" % (selection[:-5]))
 
 @app.route("/toc")
 def songs():
-    """
-    List all songs in the production folder
-    """
-
-    songs_dir = "songs/production/"
     songs = []
-    for song in listdir(songs_dir):
-        no_extension = song[:-5]
-        spaces = no_extension.replace("_", " ")
-        caps = spaces.title()
-        title, artist = caps.split("-")
+    for song in songs_data:
+        no_extension = song.filename[:-5]
+        title = song.metadata["title"]
+        artist = song.metadata["artist"]
         songs.append([no_extension, title, artist])
 
-    # return render_template("song_fixed.html")
     return render_template("toc.html",
                            songs = songs
     )
-    # songs_dir = "songs/production/"
-    # return str(listdir(songs_dir))
 
 @app.route("/songs/<title>")
 def song(title):
-    # Test with "http://0.0.0.0:8080/songs/i_will_follow_you_into_the_dark-death_cab_for_cutie"
+    try:
+        selection = next(song for song in songs_data if song.filename[:-5]==title)
+    except StopIteration:
+        return redirect("/toc")
 
-    songs_dir = "songs/production/"
-    full_title = songs_dir + title + ".song"
-    sections = []
-    metadata = {}
-    cur = None
-
-    with open(full_title, "r") as f:
-        text = f.read()
-
-    # Parse file
-    for line in text.splitlines():
-        # strip initial white space
-        line = line.rstrip()
-        # ignore blank lines
-        if not line: continue
-
-        # identify line type statements (ie "chorus:")
-        if line == line.lstrip():
-            cur = []
-            sections.append((line.strip(":"), cur))
-
-        # work with all remaining lines (content)
-        else:
-            line = line.strip()
-            # separate chords from lyrics
-            if "[" in line and "genres = " not in line:
-                chord_sections = []
-                for section in line.split("["):
-                    if "]" in section:
-                        chord, lyric = section.split("]", 1)
-                        if " " in chord:
-                            multi_chords = chord.split(" ")
-                            for m in multi_chords:
-                                chord_sections.append((m,""))
-                        else:
-                            chord_sections.append((chord, lyric))
-                    elif section:
-                        chord_sections.append(("", section))
-                line = chord_sections
-            cur.append(line)
-
-    # convert header section to dictionary
-    if sections[0][0] == "header":
-        for line in sections.pop(0)[1]:
-            key, value = line.split('=', 1)
-            metadata[key.strip()] = ast.literal_eval(value.strip())
-
-    # return str(sections)
     return render_template("song.html",
-                           title=metadata['title'],
-                           artist=metadata['artist'],
-                           sections=sections
+                           title=selection.metadata['title'],
+                           artist=selection.metadata['artist'],
+                           sections=selection.content
     )
 
 @app.route("/songs_list")
