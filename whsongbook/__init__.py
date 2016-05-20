738a5616dacb90cfdaaa5d335a88619c424dffd4
Jason Reeder <info@jasonreeder.com>
2016-05-19 17:30:13 -0700

    Add support for repeat signs in multi-chords
    Instrumental lines often need repeat signs, such as the following from
    "Soul Meets Body": "[| d:m | d:m | f | f | (x2)]". I figure the easiest
    way to deal with these is to have the parser ignore anything in
    (brackets) and just spit it out as is.

--- whsongbook/__init__.py
+++ whsongbook/__init__.py
@@ -20,30 +20,33 @@ def display_lyrics(lyrics):
     return lyrics.replace(" ", "\xA0") or "\xA0\xA0"
 
 def display_chords(chord):
-    note = chord[:1]
-    accidental = ""
-    extension = ""
-    slash = ""
-    if ":" in chord:
-        pre, extension = chord.split(":")
-    if len(chord) > 1:
-        if chord[1] == "f":
-            accidental = "b"
-        elif chord[1] == "s":
-            accidental = "#"
-    if "/" in chord:
-        slash_note = ""
-        slash_accidental = ""
-        pre, slash_chord = chord.split("/")
-        slash_note = slash_chord[:1]
-        if len(slash_chord) > 1:
-            if slash_chord[1] == "f":
-                slash_accidental = "b"
-            elif slash_chord[1] == "s":
-                slash_accidental = "#"
-        slash = "/" + slash_note.upper() + slash_accidental
+    if chord[:1] == "(":
+        ret = chord
+    else:
+        note = chord[:1]
+        accidental = ""
+        extension = ""
+        slash = ""
+        if ":" in chord:
+            pre, extension = chord.split(":")
+        if len(chord) > 1:
+            if chord[1] == "f":
+                accidental = "b"
+            elif chord[1] == "s":
+                accidental = "#"
+        if "/" in chord:
+            slash_note = ""
+            slash_accidental = ""
+            pre, slash_chord = chord.split("/")
+            slash_note = slash_chord[:1]
+            if len(slash_chord) > 1:
+                if slash_chord[1] == "f":
+                    slash_accidental = "b"
+                elif slash_chord[1] == "s":
+                    slash_accidental = "#"
+            slash = "/" + slash_note.upper() + slash_accidental
 
-    ret = note.upper() + accidental + extension + slash
+        ret = note.upper() + accidental + extension + slash
     return ret
 
 def display_section_name(name):
