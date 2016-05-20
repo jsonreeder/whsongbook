10ac025cf660575ffce855cc2c08995e4bb6c7e2
Jason Reeder <info@jasonreeder.com>
2016-05-19 17:13:00 -0700

    Add support for accidentals and slash chords
     - In LilyPond syntax, sharps are "s" and flats are "f". These need to be
    displayed as "#" and "b". This change enables that display.
     - Slash chords were being completely ignored (eg "f/bf"). This commit
     recognizes and formats them.

--- whsongbook/__init__.py
+++ whsongbook/__init__.py
@@ -20,11 +20,30 @@ def display_lyrics(lyrics):
     return lyrics.replace(" ", "\xA0") or "\xA0\xA0"
 
 def display_chords(chord):
-    note = re.sub("[0-9]|:.*", "", chord)
+    note = chord[:1]
+    accidental = ""
     extension = ""
+    slash = ""
     if ":" in chord:
         pre, extension = chord.split(":")
-    ret = note.upper() + extension
+    if len(chord) > 1:
+        if chord[1] == "f":
+            accidental = "b"
+        elif chord[1] == "s":
+            accidental = "#"
+    if "/" in chord:
+        slash_note = ""
+        slash_accidental = ""
+        pre, slash_chord = chord.split("/")
+        slash_note = slash_chord[:1]
+        if len(slash_chord) > 1:
+            if slash_chord[1] == "f":
+                slash_accidental = "b"
+            elif slash_chord[1] == "s":
+                slash_accidental = "#"
+        slash = "/" + slash_note.upper() + slash_accidental
+
+    ret = note.upper() + accidental + extension + slash
     return ret
 
 def display_section_name(name):
