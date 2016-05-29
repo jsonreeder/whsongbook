ca6a8d45783ee8c39cb663e286435311d18ba20d
Jason Reeder <info@jasonreeder.com>
2016-05-29 11:45:43 -0700

    Clean up song parsing
     - Now that I've split the parsing function in two, this file needed to
       incorporate that change.
    
     - The whole song loading process was verbose and could be simplified
       to what is in this commit.

--- whsongbook/__init__.py
+++ whsongbook/__init__.py
@@ -1,16 +1,16 @@
 from logging import FileHandler, ERROR
+from flask import Flask
 import os
 import re
 
-from flask import Flask
-
 app = Flask(__name__)
-songs_data = []
+failing_songs = []
+
+from . import parse, display
+
+songs_data = [parse.parse_file(file) for file in os.listdir("songs/production/")]
 
 from . import views
-
-from .parse import *
-from .display import *
 
 # Configure logging
 file_handler = FileHandler("app_errors.log")
@@ -20,19 +20,10 @@ app.logger.addHandler(file_handler)
 def check_type(data):
     return isinstance(data, list)
 
-def load_songs():
-    global songs_data
-    songs_dir = "songs/production/"
-    for file in os.listdir(songs_dir):
-        songs_data.append(parse_file(file))
-    return songs_data
-
-load_songs()
-
 app.jinja_env.globals.update(
     check_type = check_type,
-    display_lyrics = display_lyrics,
-    display_chords = display_chords,
-    display_section_name = display_section_name
+    display_lyrics = display.display_lyrics,
+    display_chords = display.display_chords,
+    display_section_name = display.display_section_name
 )
 
