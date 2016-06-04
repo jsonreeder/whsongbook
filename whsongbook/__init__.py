2716e6ad2cf74065c06b968fd2d91f531cc2c368
Jason Reeder <info@jasonreeder.com>
2016-06-04 11:34:39 -0700

    Add print string for songs object
    It is useful in debugging to see exactly what is contained in the parsed
    data for each song.

--- whsongbook/__init__.py
+++ whsongbook/__init__.py
@@ -1,11 +1,11 @@
-# from logging import FileHandler, ERROR
-from flask import Flask
-from collections import defaultdict
 import os
 import re
 import logging
+from collections import defaultdict
+from flask import Flask
 
-logging.basicConfig(filename="errors.log", level=logging.ERROR)
+# logging.basicConfig(filename="errors.log", level=logging.ERROR)
+logging.basicConfig(filename="errors.log", level=logging.DEBUG)
 app = Flask(__name__)
 failing_songs = []
 
@@ -13,12 +13,17 @@ from . import parse, display
 
 # Load songs
 songs_data = [parse.parse_file(file) for file in os.listdir("songs/production/")]
+logging.debug("Songs Data:\n")
+for i in songs_data:
+    logging.debug(i)
+
 
 # Die if any songs fail to parse
 if failing_songs:
     import sys
     sys.exit(1)
 
+# Build list of artists and their songs
 artists = defaultdict(list)
 for s in songs_data:
     artists[s.metadata["artist"]].append(s.metadata["title"])
