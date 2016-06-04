5df470608eac06e18950759d57c056788860e48d
Jason Reeder <info@jasonreeder.com>
2016-06-04 12:07:29 -0700

    Add new songs view

--- whsongbook/views.py
+++ whsongbook/views.py
@@ -17,8 +17,8 @@ def random():
     selection = choice(songs_data).filename
     return redirect("/songs/%s" % (selection[:-5]))
 
-@app.route("/browse")
-def browse():
+@app.route("/browse_old")
+def browse_old():
     songs = []
     for song in songs_data:
         no_extension = song.filename[:-5]
@@ -30,8 +30,20 @@ def browse():
                            songs = sorted(songs, key=lambda song: song[1])
     )
 
-@app.route("/songs/<title>")
-def song(title):
+@app.route("/browse")
+def browse():
+    songs = []
+    for song in songs_data:
+        title = song.metadata["title"]
+        artist = song.metadata["artist"]
+        link = "%s__-__%s" % (title.replace(" ", "_"), artist.replace(" ", "_"))
+        songs.append([link, title, artist])
+
+    return render_template("browse.html",
+                           songs = sorted(songs, key=lambda song: song[1])
+    )
+@app.route("/songs_old/<title>")
+def song_old(title):
 
     # test for urls to songs that do not exist
     try:
@@ -46,6 +58,27 @@ def song(title):
                            artist=selection.metadata['artist'],
                            sections=selection.content
     )
+
+@app.route("/songs/<title_artist>")
+def song(title_artist):
+
+    title_nospace, artist_nospace = title_artist.split("__-__")
+    title = title_nospace.replace("_", " ")
+    artist = artist_nospace.replace("_", " ")
+
+    # test for urls to songs that do not exist
+    try:
+        selection = next(song for song in songs_data if song.metadata["title"]==title and song.metadata["artist"]==artist)
+    except StopIteration:
+        return redirect("/browse")
+
+    return render_template("song.html",
+                           filename=selection.filename,
+                           title=title,
+                           artist=artist,
+                           sections=selection.content
+    )
+
 
 @app.route("/artists/<name>")
 def artist(name):
