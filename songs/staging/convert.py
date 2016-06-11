72bbea09ecf005f4e56ed2e38af89185babb9a11
Jason Reeder <info@jasonreeder.com>
2016-06-11 13:31:09 -0700

    Refactor

--- songs/staging/convert.py
+++ songs/staging/convert.py
@@ -1,18 +1,69 @@
-# Initialize globals
-outfile = "temp_out.song"
+# Initialize gloabls
+infile = "junk_in.txt"
+outfile = "junk_out.txt"
+out_text = ""
 
-# Define heper funcions
-def parse_header(text):
-    return text
+# Read file
+with open(infile, "r") as f:
+    text = f.read()
 
-def run():
-    with open("junk_processing.txt", "r") as f:
-        text = f.read()
+    # Split into parts
+    sections = text.split("\n\n")
+    parts = []
+    c = 1
+    for s in sections:
+        name = "section_%s" % (c)
+        lines = s.splitlines()
+        parts.append({name: lines})
+        c += 1
 
-        out_text = parse_header(text)
+    # Parse parts
+    parsed_parts = []
+    for p in parts:
+        for section_name, content in p.items():
+            type = "verse"
 
-        with open(outfile, "w") as o:
-            o.write(out_text)
+            # Parse header
+            if section_name == "section_1":
+                type = "header"
 
-# Run
-run()
+            # Parse choruses
+            stripped = [c.strip() for c in content]
+            if "(Chorus)" in stripped:
+                type = "chorus"
+                content = ""
+
+            parsed_parts.append((type, content))
+
+    # Format
+    for p in parsed_parts:
+        type = p[0]
+        content = p[1]
+
+        if type == "header":
+            out_text += "%s:\n" % (type)
+            header_types = {0: "title", 1: "artist", 2: "capo"}
+            for i, c in enumerate(content):
+                out_text += "    %s = \"%s\"\n" % (header_types[i], c)
+
+        # Chorus
+        elif content == "":
+            out_text += "%s\n" % type
+
+
+
+        # Others
+        else:
+            out_text += "%s:\n" % type
+            for line in content:
+                out_text += "    %s\n" % line
+        out_text += "\n"
+
+
+# Write output
+# with open(outfile, "w") as f:
+    # for p in parts:
+    #     line = str(p) + "\n\n"
+    #     f.write(line)
+
+print(out_text)
