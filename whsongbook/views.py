5d265c6502d41d73b03a62c4cc71ea1af5ca96f7
Jason Reeder <info@jasonreeder.com>
2016-10-09 22:24:13 -0700

    Add search view

--- whsongbook/views.py
+++ whsongbook/views.py
@@ -7,7 +7,7 @@ This module defines the endpoints of the Flask app.
 from random import choice
 from collections import defaultdict
 from flask import render_template, redirect
-from . import app, songs_data, artists_data, tags_data, languages_data, display
+from . import app, songs_data, artists_data, tags_data, languages_data, display, ix
 
 
 @app.route("/")
@@ -237,3 +237,17 @@ def display_songs_data():
     """
 
     return str([song.get_json() for song in songs_data])
+
+
+@app.route("/search")
+def search():
+    """
+    Search for songs.
+    """
+
+    from whoosh.qparser import QueryParser
+    with ix.searcher() as searcher:
+        query = QueryParser("title", ix.schema).parse("document")
+        results = searcher.search(query)
+
+    return render_template("search.html", results=results)
